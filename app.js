cb806cfabb5ace473e8ef316a5f90120292282e5
Usama Iqbal <mohamedusama881@gmail.com>
2019-07-12 12:18:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction(){
+
+    var name = prompt("Your Name: ")
+    document.getElementById("demo").innerHTML="Hello "+name;
+
+}
+//document.write(5+9);
+
+//One Statement Many Variable
+//var person = "John Doe", carName = "Volvo", price = 200;
